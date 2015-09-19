7079246b599b5cbbbbb1cc25bc5705499460d078
Chris Wood <chris.wood@ticketmaster.co.uk>
2015-09-19 19:03:51 +0100

    Adding ufw port openings and sorting helloworld.py not_if

--- cookbooks/webapp/recipes/app.rb
+++ cookbooks/webapp/recipes/app.rb
@@ -32,6 +32,12 @@ end
 
 execute 'start helloworld' do
   command 'nohup python2.7 /opt/helloworld/bin/helloworld.py > /opt/helloworld/log/stdout.log 2>&1 &'
-  not_if 'ps -ef | grep helloworld.py'
+  not_if 'ps -ef | grep -v grep | grep helloworld.py'
+  action :run
+end
+
+execute 'open firewall' do
+  command 'ufw allow 8888/tcp'
+  not_if 'ufw status | grep 8888 | grep ALLOW'
   action :run
 end
